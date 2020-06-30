c91ae70e6576b42b4447f0149301508fd3ef55e2
xixi2 <2821893735@qq.com>
2020-06-30 08:42:58 +0800

    修改app.py

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ from tornado.options import define, parse_command_line, options
 define("port", type=int, default=8888, help="服务端监听的端口")
 parse_command_line()
 
-from handlers.fact_handler import fact_routing
+from handlers.fact import fact_routing
 from handlers.use_db import use_db_routing
 
 
